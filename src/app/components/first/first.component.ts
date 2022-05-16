5de50633964ce5f4a57dd677a25358ec1a2c61fc
aymen2782 <aymen.sellaouti@gmail.com>
2022-05-16 15:15:44 +0100

    binding interpolation property and event

---
+++ src/app/components/first/first.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css'],
+})
+export class FirstComponent implements OnInit {
+  name = 'aymen';
+  isHidden = true;
+  buttonName = 'show';
+  age = '';
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.name = 'yosri';
+    }, 3000);
+    /*     setInterval(() => {
+      this.isHidden = !this.isHidden;
+    }, 2000); */
+  }
+  changeAge(newValue: string) {
+    this.age = newValue;
+  }
+  showHide() {
+    this.isHidden = !this.isHidden;
+    this.buttonName == 'show'
+      ? (this.buttonName = 'hide')
+      : (this.buttonName = 'show');
+  }
+}
